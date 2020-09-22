d38fe2006683cf1c9a8248e0871c27cf4c6b3193
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-09-22 15:02:08 +0200

    exercise B5

---
+++ exB5/ExB5.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class ExB5 {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		String answer = "Y";
+	
+		while (answer.equals("Y")){
+			System.out.println("Welcome !");
+			System.out.println("Would you like to be greeted again ? (Y/N) ");
+			answer = input.nextLine();
+			} 
+		}
+}
