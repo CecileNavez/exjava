4b294579f2d4031de3a888ddb182d83bffd1ccba
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-09-22 10:01:09 +0200

    adding exercise A1

---
+++ exA1/ExA1.java
@@ -0,0 +1,16 @@
+
+public class ExA1 {
+	
+	public static void main(String[] args) {
+		String title = "Learning Java";
+		int numberofdays = 5;
+		double priceperday = 800;
+		Boolean priorknowledge = false;
+		
+		System.out.println("The title of the course is '" + title + "', the course duration is " + numberofdays + " days, the price per day is " + priceperday + "EUR and it is " + priorknowledge + " to think that you need prerequisite to follow the course.");
+	}
+
+
+}
+
+
