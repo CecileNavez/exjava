473a5c646569076110ce9bbc7e5f7c2b6b2633d7
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-09-23 09:53:43 +0200

    correction Exercise C1

--- exC1/ExC1.java
+++ exC1/ExC1.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 
 public class ExC1 {
 	
-	public static void printInfo() {
-		String title;
+	public static void printInfo(String title, int numberofdays, double priceperday, boolean priorknowledge) {
+				
+		System.out.println("The title of the course is '" + title + "'.");
+		System.out.println("The course duration is " + numberofdays + " days.");
+		System.out.println("The price per day is " + priceperday + " EUR.");
+		System.out.println("It is " + priorknowledge + " to think that you need prerequisite to follow the course.");
+		
+		}
+	
+	public static void main(String[] args) {
+	
+	String title;
 		int numberofdays;
 		double priceperday;
 		boolean priorknowledge;
 				
-		ArrayList<String> instructorNames = new ArrayList<String>();
-		instructorNames.add("Sandy");
-		instructorNames.add("Cecile");
-		instructorNames.add("Philippe");
-		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the title : ");
 		title = input.nextLine();
@@ -27,18 +32,7 @@ public class ExC1 {
 		System.out.println("A knowledge is pre-required (false/true) ? : ");
 		priorknowledge = input.nextBoolean();
 		
-			
-		
-		System.out.println("The title of the course is '" + title + "'.");
-		System.out.println("The course duration is " + numberofdays + " days.");
-		System.out.println("The price per day is " + priceperday + " EUR.");
-		System.out.println("Number of instructors for this course : " + instructorNames.size() + ".");
-		System.out.println("It is " + priorknowledge + " to think that you need prerequisite to follow the course.");
-		
-		}
-	
-	public static void main(String[] args) {
-		ExC1.printInfo();
+		ExC1.printInfo(title, numberofdays, priceperday, priorknowledge);
 		
 	}
 }
