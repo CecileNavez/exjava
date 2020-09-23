f1396cc4162b0def3800c93f2b6b5a1b7044c427
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-09-23 11:10:02 +0200

    Exercice C3 corrected

--- exC3/ExC3.java
+++ exC3/ExC3.java
@@ -6,8 +6,12 @@ import java.util.ArrayList;
 public class ExC3 {
 	
 	public static void main(String[] args) {
-		List<Integer> listOfIntegers = new List<Integer>();
+		ArrayList<Integer> listOfIntegers = new ArrayList<Integer>();
 		
+			listOfIntegers.add(5);
+			listOfIntegers.add(6);
+			listOfIntegers.add(3);
+	
 		double average = DoMath.calculateAverage(listOfIntegers);
 		System.out.println("The average is " + average);
 		
