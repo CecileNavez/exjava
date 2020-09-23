f1b05001d9df9c2d3b74a33960f4f690f6005029
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-09-23 10:01:03 +0200

    Exercise C2

---
+++ exC2/ExC2.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExC2 {
+	
+	public static void printInfo(String title, int numberofdays, double priceperday, boolean priorknowledge) {
+				
+		System.out.println("The title of the course is '" + title + "'.");
+		System.out.println("The course duration is " + numberofdays + " days.");
+		System.out.println("The price per day is " + priceperday + " EUR.");
+		System.out.println("It is " + priorknowledge + " to think that you need prerequisite to follow the course.");
+		System.out.println("The total price of the course is " + totalPrice(numberofdays, priceperday, priorknowledge) + " EUR.");
+		}
+		
+	public static double totalPrice(int a, double b, boolean priorknowledge){ // a = number of days - b = price per day
+	
+		double c = a * b; // c= total price of the course without VAT
+	
+		if (a > 3 && priorknowledge) { 
+		
+			return c;
+			
+			} else {
+			
+			return c *= 1.21;
+			
+			}
+		
+		}
+	
+	public static void main(String[] args) {
+		String title;
+		int numberofdays;
+		double priceperday;
+		boolean priorknowledge;
+				
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the title : ");
+		title = input.nextLine();
+		
+		System.out.println("Enter the number of days : ");
+		numberofdays = input.nextInt();
+		
+		System.out.println("Enter the price per day : ");
+		priceperday = input.nextDouble();
+		
+		System.out.println("A knowledge is pre-required (false/true) ? : ");
+		priorknowledge = input.nextBoolean();
+		
+		ExC2.printInfo(title, numberofdays, priceperday, priorknowledge);
+		
+	}
+}
